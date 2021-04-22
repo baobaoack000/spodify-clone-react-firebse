72c9400c63b15ddc1bbae650899bebef47f16ae1
Nguyen Gia Bao <Baobaoack000@gmail.com>
2021-04-22 20:22:07 +0700

    create sketch login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-
+import Login from './Login'
 import './App.css';
 // eslint-disable-next-line no-console
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
+      <Login/>
       {/* Spodify logo */}
       
       {/* Login button */}
---
+++ src/Login.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Login.css"
+// eslint-disable-next-line no-console
+function Login() {
+    return (
+        <div className="login">
+            <h1>This is login</h1>   
+            {/* spodify logo */}
+            <img src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_White.png" alt=""/>
+            {/* login with spotify button */}
+            <a>LOGIN WITH SPODIFY</a>
+        </div>
+    )
+}
+
+export default Login
