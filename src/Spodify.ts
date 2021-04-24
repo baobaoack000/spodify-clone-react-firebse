421ad804875a25a31fbb9cc0986fa350eabeca71
Nguyen Gia Bao <Baobaoack000@gmail.com>
2021-04-24 16:10:22 +0700

    fix interpolation uri naming in login-url in Spodify.ts

--- src/Spodify.ts
+++ src/Spodify.ts
@@ -5,7 +5,7 @@
 export const authorization =
 "https://accounts.spotify.com/authorize";
 //return home
-const redirectUri = "https://localhost:3000/";
+const redirectUri = "http://localhost:3000/";
 //dashboard
 const clientID ="cadeb5b192254c6b9c6bc88fb9be5632";
 const scopes = [
@@ -18,8 +18,8 @@ const scopes = [
 // Combination for Url
 export const login=
 `${authorization}
-?clientid=${clientID}
-&redirectUri=${redirectUri}
+?client_id=${clientID}
+&redirect_uri=${redirectUri}
 &scope=${scopes.join("%20")}
 &response_type=token
 &show_dialog=true`; //join scope for login
